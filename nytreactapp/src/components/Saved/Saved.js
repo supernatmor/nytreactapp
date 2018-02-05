cdbaf7c0b797cdeface0e4bb4aca99cf68725629
Nate Morgan <natmor88@gmail.com>
2018-02-05 14:33:34 -0500

    updated classnames to jsx

--- nytreactapp/src/components/Saved/Saved.js
+++ nytreactapp/src/components/Saved/Saved.js
@@ -1,10 +1,9 @@
-import { Col, Row, Container } from "../../components/Grid";
 import React from "react";
 
 const Saved = () => {
 
 <div className="row">
-    <div class="card bg-light mb-3 col-lg-12">
+    <div className="card bg-light mb-3 col-lg-12">
         <div class="card-header">Saved Articles</div>
         <div class="card-body">
             <h5 class="card-title">Light card title</h5>
--- nytreactapp/src/components/Search/Search.js
+++ nytreactapp/src/components/Search/Search.js
@@ -1,13 +1,12 @@
-import { Col, Row, Container } from "../../components/Grid";
 import React from "react";
 
 const Search = () => {
     <div className="row">
-        <div class="card bg-light mb-3 col-lg-12">
-            <div class="card-header">Search Articles</div>
-            <div class="card-body">
-                <h5 class="card-title">Light card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <div className="card bg-light mb-3 col-lg-12">
+            <div className="card-header">Search Articles</div>
+            <div className="card-body">
+                <h5 className="card-title">Light card title</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             </div>
         </div>
     </div>
